40bb9b5ee28b0d04f80f91ffa400b7e78d06ecc7
pengfuguo <vaemc520@qq.com>
2022-12-11 12:26:20 +0800

    初步完成

--- online_serial_core/index.js
+++ online_serial_core/index.js
@@ -1,9 +1,21 @@
 
+import WebSocket, { WebSocketServer } from 'ws';
 import { SerialPort } from 'serialport'
 import { ReadlineParser } from '@serialport/parser-readline'
+const wss = new WebSocketServer({ port: 8080 });
+wss.on('connection', (ws) => {
+});
+
 const port = new SerialPort({
     path: 'COM6',
     baudRate: 115200,
 })
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
-parser.on('data', console.log)
+parser.on('data', data => {
+    console.info(data);
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
+})
