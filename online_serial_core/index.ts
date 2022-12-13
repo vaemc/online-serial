5d3e1dcef3ff1cb3cdfc9ddfcecad902225d8890
pengfuguo <vaemc520@qq.com>
2022-12-14 00:20:32 +0800

    更改目录结构

--- online_serial_core/index.ts
+++ online_serial_core/index.ts
@@ -1,6 +1,6 @@
 import express, { Express} from 'express';
 import cors from 'cors';
-import { serialRouter } from './serial/http'
+import { serialRouter } from './http/serial'
 import dotenv from 'dotenv';
 dotenv.config();
 
